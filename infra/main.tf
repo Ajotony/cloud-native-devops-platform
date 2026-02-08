bf8f8927a33eac311fc8b0d8ff3f0c8a130641fe
Anthony Ajibola-Ajo <ajotony15@gmail.com>
2026-02-08 18:19:46 +0100

    Add EC2 instance with dynamic AMI and security group

--- infra/main.tf
+++ infra/main.tf
@@ -10,3 +10,65 @@ terraform {
 provider "aws" {
   region = var.aws_region
 }
+
+resource "aws_security_group" "firewall"{
+  name  = "cloud-native-firewall"
+  description  = "Allow SSH and application traffic"
+
+  ingress {
+    description  = "SSH access"
+    from_port  = 22
+    to_port  = 22
+    protocol  = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    description = "Application traffic"
+    from_port  = 5000
+    to_port  = 5000
+    protocol  = "tcp"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+
+  egress {
+    description  = "Allow all outbound traffic"
+    from_port  = 0
+    to_port  = 0
+    protocol  = "-1"
+    cidr_blocks  = ["0.0.0.0/0"]
+  }
+}
+
+
+data "aws_ami" "ubuntu"{
+  most_recent = true
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd/ubuntu-jammy-22.04-amd64-server-*"]
+  }
+
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+
+  owners = ["099720109477"] # Canonical
+}
+
+
+resource "aws_instance" "ec2" {
+  ami  = data.aws_ami.ubuntu.id
+  instance_type  = "t2.micro"
+
+  key_name  = var.ssh_key_name
+
+  vpc_security_group_ids  = [
+    aws_security_group.firewall.id
+  ]
+
+  tags  = {
+    Name  = "cloud-native-web-server"
+  }
+}
--- infra/variables.tf
+++ infra/variables.tf
@@ -1,5 +1,10 @@
 variable "aws_region" {
-  description = "AWS region where resources will be created"
+  description = "AWS region where the resources will be created"
   type  = string
   default  = "us-east-1"
+}
+
+variable "ssh_key_name" {
+  description  = "Name of the existing AWS key pair for ssh access"
+  type  = string
 }
